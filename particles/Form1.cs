4c5d598775780497d5011df7a969c246cc276065
AshHvostovsky <ash.stdw@gmail.com>
2024-05-09 16:45:14 +0800

    Добавьте файлы проекта.

---
+++ particles/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace particles
+{
+    public partial class Form1 : Form
+    {
+        List<Particle> particlesList = new List<Particle>();
+        public Form1()
+        {
+            InitializeComponent();
+
+            
+
+
+            // привязал изображение
+            picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
+
+            // генерирую 500 частиц
+            for (var i = 0; i < 500; ++i)
+            {
+                var particle = new Particle();
+                // переношу частицы в центр изображения
+                particle.X = picDisplay.Image.Width / 2;
+                particle.Y = picDisplay.Image.Height / 2;
+                // добавляю список
+                particlesList.Add(particle);
+            }
+        }
+        int counter = 0; // добавлю счетчик чтобы считать вызовы функции
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            counter++; // увеличиваю значение счетчика каждый вызов
+            using (var g = Graphics.FromImage(picDisplay.Image))
+            {
+                g.Clear(Color.White); // добавил очистку
+                foreach (var particle in particlesList)
+                {
+                    particle.Draw(g);
+                }
+            }
+
+            // обновить picDisplay
+            picDisplay.Invalidate();
+        }
+    }
+}
